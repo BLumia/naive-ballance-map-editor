e07d5a736f05e65ea6bfaff3b1a061efc815adbe
Gary Wang <wzc782970009@gmail.com>
2020-08-20 14:15:53 +0800

    feat: no longer a blank screen when running (but still useless)

--- Scripts/gridmap.gd
+++ Scripts/gridmap.gd
@@ -1,10 +1,10 @@
 extends Spatial
 
+onready var ray = $SelectionTool/RayCast
+onready var gridmap_node = $GridMap
+onready var cursor_object = $SelectionTool/CursorAt
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var gridmap_node = get_node("GridMap")
+var ray_length = 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,8 +14,23 @@ func _ready():
 		var name = meshlib.get_item_name(gridmap_node.get_cell_item(cell.x, cell.y, cell.z))
 		var ori = gridmap_node.get_cell_item_orientation(cell.x, cell.y, cell.z)
 		print(String(cell) + ": " + name + ", " + String(ori))
-	pass # Replace with function body.
 
+
+func _physics_process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = get_viewport().get_camera().project_ray_origin(mouse_pos)
+	var to = get_viewport().get_camera().project_ray_normal(mouse_pos) * ray_length
+	
+	ray.transform.origin = from
+	ray.cast_to = to
+	ray.force_raycast_update()
+	
+	var point = gridmap_node.world_to_map(ray.get_collision_point())
+	var tile = gridmap_node.get_cell_item(point.x, point.y, point.z)
+	
+	var selection_pos = gridmap_node.map_to_world(point.x, point.y, point.z)
+	cursor_object.translation = selection_pos
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
