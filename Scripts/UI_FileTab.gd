ee57967c5ddf37b6f637335e37a9cf6ccdbe5146
Gary Wang <wzc782970009@gmail.com>
2020-09-07 23:38:27 +0800

    feat: add a confirm dialog...

--- Scripts/UI_FileTab.gd
+++ Scripts/UI_FileTab.gd
@@ -5,6 +5,7 @@ onready var gridmap_node = $"/root/Spatial/GridMap"
 onready var uiRoot = $"/root/Spatial/UI"
 onready var loadDlg = FileDialog.new()
 onready var saveDlg = FileDialog.new()
+onready var confirmDlg = ConfirmationDialog.new()
 
 const LevelData = preload("res://Scripts/LevelData.gd")
 
@@ -26,6 +27,11 @@ func _ready():
 	uiRoot.call_deferred("add_child", saveDlg)
 	saveDlg.connect("file_selected", self, "_on_SaveButton_DialogConfirmed")
 
+	confirmDlg.popup_exclusive = true
+	confirmDlg.dialog_text = "Any unsaved changes will lost, continue?"
+	uiRoot.call_deferred("add_child", confirmDlg)
+	confirmDlg.connect("confirmed", self, "_on_NewButton_DialogConfirmed")
+
 
 func _on_SaveButton_pressed():
 	saveDlg.popup_centered_ratio()
@@ -33,6 +39,10 @@ func _on_SaveButton_pressed():
 
 func _on_LoadButton_pressed():
 	loadDlg.popup_centered_ratio()
+
+
+func _on_NewButton_pressed():
+	confirmDlg.popup_centered()
 
 
 func _on_SaveButton_DialogConfirmed(path: String):
@@ -48,3 +58,7 @@ func _on_LoadButton_DialogConfirmed(path: String):
 	var succ = levelData.parse_from_bme_file(path)
 	print(succ)
 	levelData.set_gridmap_from_level_data(gridmap_node)
+
+
+func _on_NewButton_DialogConfirmed():
+	gridmap_node.clear()
